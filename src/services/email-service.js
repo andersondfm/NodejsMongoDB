494a426bf6d2c27f15dea3c4d0d9fe3448e2b5f9
Andersondfm <anderson.migloranza@gmail.com>
2019-01-16 13:12:12 -0200

    Anderson

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,6 +1,6 @@
 'use strict';
 var config = require('../config');
-var sendgrid = require('sendgrid')('Coloca Aqui Sua Key Aqui');
+var sendgrid = require('sendgrid')('Coloca Aqui Sua Key');
 
 exports.send = async (to, subject, body) => {
     sendgrid.send({
